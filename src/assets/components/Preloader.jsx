4f468efa642d6b11bd53a1c8ddde93734f919194
Shubham-391 <subhamy1234@gmail.com>
2024-01-26 16:39:54 +0530

    preloader

--- src/assets/components/Preloader.jsx
+++ src/assets/components/Preloader.jsx
@@ -12,7 +12,7 @@ function Preloader() {
         id="Preloader"
         className="h-screen bg-white flex flex-col gap-4 justify-center items-center fixed top-0 left-0 w-full z-30 "
       >
-        <img className="animate-bounce w-[100px]" src={logo} alt="logo" />
+        <img rel="preload" className="animate-bounce w-[100px]" src={logo} alt="logo" />
         <div className="loader w-[50px] h-[50px] relative border-[5px] border-solid border-[#555] border-t-[#f9f9f9] rounded-[50%] animate-spin"></div>
       </div>
     </>
